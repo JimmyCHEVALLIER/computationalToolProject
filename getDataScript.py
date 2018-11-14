aeda03e378b7072c12fbc5b7bd43227b9ebe8038
s181813 <jimmy.chevallier@supinfo.com>
2018-11-14 10:00:34 +0100

    get category function

--- getDataScript.py
+++ getDataScript.py
@@ -1,6 +1,7 @@
 import wikipedia
 from bs4 import BeautifulSoup
 import re
+import urllib.parse
 
 def getWikiArticle(strTitle):
     res = wikipedia.WikipediaPage(title=strTitle) # get HTML content of the article
@@ -10,6 +11,35 @@ def getWikiArticle(strTitle):
     contentDict["Plot"] = res.section("Plot") # append plot
     return contentDict
 
+def getWikiCategorie(strTitle):
+    res = wikipedia.WikipediaPage(title=strTitle)  # get HTML content of the article 
+    soup = BeautifulSoup(res.html(), 'html.parser')
+    tags = [tag('a') for tag in soup.find_all('li')]
+    data = []
+    for tag in tags:
+        if len(tag) == 0:
+            continue
+        tag = tag[0]
+        if "href" in tag.attrs and "title" in tag.attrs:
+            if tag.attrs['title'].startswith("List") or tag.attrs['title'].startswith("Template") or tag.attrs['title'].startswith("Edit"):
+                pass
+            else:
+                #print("movie ->", tag.attrs['title'],tag.attrs['href'].rsplit('/', 1)[-1])
+                data.append(urllib.parse.unquote(tag.attrs['href'].rsplit('/', 1)[-1]))
+    return data
 
-print(getWikiArticle('Atragon'))
+#print(getWikiArticle('BABO'))
+#res = wikipedia.WikipediaPage(title="Bitters_and_Blue_Ruin")  # get HTML content of the article 
+#print(getWikiCategorie('List of films: B'))
 
+# create a big list of all name and store it
+
+# create a file for each list with the data
+
+count = 0
+for elem in getWikiCategorie('List of films: B'):
+    print(count)
+    res = wikipedia.WikipediaPage(title=elem)
+    count+=1
+
+print("done")
